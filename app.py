41fbc13f835beac0b8fc48568b87df1f2f82d111
Sunil Subramanya <84517045+sunil-trainingdata@users.noreply.github.com>
2023-04-19 19:59:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 from dash import Dash, html, dcc
-import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
@@ -18,18 +14,17 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(className='row', children=[
-    dbc.Row([
-        dbc.Col(
-            html.H1(children='Hello Dash Updated',id='f7f9f0e1-05b1-4547-8590-ffbbe271c4d8'),
-            width={"size": 12},
-        ), 
-        dbc.Col(
-            dcc.Graph(id='example-graph', figure={'data': [{"alignmentgroup":"True","hovertemplate":"City=SF<br>Fruit=%{x}<br>Amount=%{y}<extra></extra>","legendgroup":"SF","marker":{"color":"#636efa","pattern":{"shape":""}},"name":"Dataset #1","offsetgroup":"SF","orientation":"v","showlegend":true,"textposition":"auto","type":"bar","x":["Apples","Oranges","Bananas"],"xaxis":"x","y":[4,1,2],"yaxis":"y"},{"alignmentgroup":"True","hovertemplate":"City=Montreal<br>Fruit=%{x}<br>Amount=%{y}<extra></extra>","legendgroup":"Montreal","marker":{"color":"#EF553B","pattern":{"shape":""}},"name":"Dataset #2","offsetgroup":"Montreal","orientation":"v","showlegend":true,"textposition":"auto","type":"bar","x":["Apples","Oranges","Bananas"],"xaxis":"x","y":[2,4,5],"yaxis":"y"}]}),
-            width={"size": 12},
-        ),
-    ])
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
 if __name__ == '__main__':
